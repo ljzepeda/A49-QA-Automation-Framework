f5e8fa28bb8580274a3ed1cbad89b9265f5c4807
Lirio J. Zepeda <127067223+ljzepeda@users.noreply.github.com>
2023-10-23 20:23:43 -0700

    L19-data-providers

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,77 @@
+import com.github.hemanthsridhar.CSVUtils;
+import com.github.hemanthsridhar.lib.ExtUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.BeforeSuite;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.time.Duration;
+import java.util.UUID;
 
 public class BaseTest {
+    public WebDriver driver = null;
+    //    public String url = "https://qa.koel.app/";
+    public String url;
 
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod
+    @Parameters({"BaseURL"})
+    public void launchBrowser(String baseURL) {
+        //Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--start-maximized");
+        url=baseURL;
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterMethod
+    public void closeBrowser() {
+        driver.quit();
+    }
+
+    public void navigateToPage() {
+        driver.get(url);
+    }
+
+    public void provideEmail(String email) {
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
+
+    public void providePassword(String password) {
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+
+    public void clickSubmit() {
+        WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+        submit.click();
+    }
+    @DataProvider(name ="IncorrectLoginData")
+    public static Object[][] getDataFromDataProviders() {
+        return new Object[][]{
+                {"notExisting@email.com", "notExistingPassword"},
+                {"demo@class.com", ""},
+                {"", ""}
+        };
+    }
+    @DataProvider(name = "csvData")
+    public Object[][] csvDataRead() throws Exception {
+        String path ="src/test/resources/cred.csv";
+        ExtUtils ext = new CSVUtils(path,false);
+        return ext.parseData();
     }
 }
